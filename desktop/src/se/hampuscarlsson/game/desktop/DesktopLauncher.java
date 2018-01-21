198360dad42fe1e4f60b2c482e8f08eed0da1c68
Zumpo <carlsson889@gmail.com>
2018-01-21 19:47:36 +0100

    Init

---
+++ desktop/src/se/hampuscarlsson/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.hampuscarlsson.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.hampuscarlsson.game.ECSGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ECSGame(), config);
+	}
+}
